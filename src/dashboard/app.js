0a00ac9e9260300ba252a2998e3daab1b27e8b24
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-09-17 14:05:01 +1200

    Added site

--- src/dashboard/app.js
+++ src/dashboard/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
-const path = require("node:path");
+//const path = require("node:path");
 
 module.exports = async (client) => {
-    const app = await express();
+    const app = express();
 
+    /*
     app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'ejs');
+    app.engine('html', require('html')._express);
+    app.set('view engine', 'html');
 
     app.get('/', function(req, res){
         res.render('index');
     });
+    */
+
+    app.use(express.static('src/dashboard/views/'));
 
     app.listen(3000);
 
-    console.log("Website online, listening at http://localhost:3000/");
+    console.log("Website online, listening on http://localhost:3000/");
 }
