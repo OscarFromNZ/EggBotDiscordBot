160023051890c7c0771f3fc44b8f0e4ab29be67e
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-10-11 20:22:59 +1300

    feat: added mongodb saving yay

--- src/commands/embed/execute/create.js
+++ src/commands/embed/execute/create.js
@@ -2,14 +2,16 @@
     * @INFO Create an embed in a guild
 */
 
-const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder, componentType } = require('discord.js');
+const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder, ClientPresence } = require('discord.js');
 
 module.exports = {
     async execute(client, interaction) {
+        let guildDoc = await client.functions.getOrCreateGuild(client, interaction.guild.id);
+
         // Create the modal
         const modal = new ModalBuilder()
             .setCustomId('embedBuilder')
-            .setTitle('Embed Builder Interactive Setup');
+            .setTitle('Embed Builder Interactive Setup')
 
         // Add components to modal
 
@@ -19,26 +21,30 @@ module.exports = {
             // The label is the prompt the user sees for this input
             .setLabel("What should the color of the embed be? (hex)")
             // Short means only a single line of text
-            .setStyle(TextInputStyle.Short);
+            .setStyle(TextInputStyle.Short)
+            .setRequired(false);
 
         const embedTitle = new TextInputBuilder()
             .setCustomId('embedTitle')
             // The label is the prompt the user sees for this input
             .setLabel("What should the title be?")
             // Short means only a single line of text
-            .setStyle(TextInputStyle.Short);
+            .setStyle(TextInputStyle.Short)
+            .setRequired(false);
 
         const embedDescription = new TextInputBuilder()
             .setCustomId('embedDescription')
             .setLabel("What should the description be?")
             // Paragraph means multiple lines of text.
-            .setStyle(TextInputStyle.Paragraph);
+            .setStyle(TextInputStyle.Paragraph)
+            .setRequired(false);
 
         const embedImage = new TextInputBuilder()
             .setCustomId('embedImage')
             .setLabel("What should the image be? (link)")
             // Paragraph means multiple lines of text.
-            .setStyle(TextInputStyle.Short);
+            .setStyle(TextInputStyle.Short)
+            .setRequired(false);
 
         /*         
         const embedFooter = new TextInputBuilder()
@@ -52,7 +58,8 @@ module.exports = {
             .setCustomId('embedThumbnail')
             .setLabel("What should the thumbnail be? (link)")
             // Paragraph means multiple lines of text.
-            .setStyle(TextInputStyle.Short);
+            .setStyle(TextInputStyle.Short)
+            .setRequired(false);
 
 
         // An action row only holds one text input,
@@ -73,30 +80,46 @@ module.exports = {
         const filter = (interaction) => interaction.customId === 'embedBuilder';
         await interaction.awaitModalSubmit({ filter, time: 30_000 })
             .then(async interaction => {
-                /*
-                let emb = new EmbedBuilder()
-                    .setColor(interaction.components[0].components[0].value)
-                    .setTitle(interaction.components[1].components[0].value)
-                    .setDescription(interaction.components[2].components[0].value)
-                    .setImage(interaction.components[3].components[0].value)
-                    .setThumbnail(interaction.components[4].components[0].value)
-                await interaction.reply({ embeds: [emb] });
-                */
 
-                /*
-                let m = await interaction.editReply(
+                let emb = new EmbedBuilder()
+                    .setColor(interaction.components[0].components[0].value === "" ? undefined : interaction.components[0].components[0].value)
+                    .setTitle(interaction.components[1].components[0].value === "" ? undefined : interaction.components[1].components[0].value)
+                    .setDescription(interaction.components[2].components[0].value === "" ? undefined : interaction.components[2].components[0].value)
+                    .setImage(interaction.components[3].components[0].value === "" ? undefined : interaction.components[3].components[0].value)
+                    .setThumbnail(interaction.components[4].components[0].value === "" ? undefined : interaction.components[4].components[0].value);
+
+                await interaction.reply(
                     {
-                        content: `What would you like to name this embed?`,
+                        content: `Data recorded successfully`,
                     }
                 );
 
-                const collector = interaction.channel.createMessageCollector({ filter, time: 15000 });
+                let m = await interaction.followUp(
+                    {
+                        content: `What would you like to name this embed? (message)`,
+                    }
+                );
 
-                collector.on('collect', m => {
-                    console.log(`Collected ${m.content}`);
+                const collector = await interaction.channel.createMessageCollector({ time: 15000 });
+
+                collector.on('collect', async (m) => {
+                    if (m.author.bot) return;
+                    if (m.author.id !== interaction.user.id) return;
+
+                    await interaction.followUp(
+                        {
+                            content: `Sweet! Your embed, "${m.content}", has been saved successfully`,
+                            embeds: [ emb ]
+                        }
+                    );
+
+                    guildDoc.embeds ? guildDoc.embeds[m.content] = emb : guildDoc.embeds = { [m.content]: emb };
+                    await client.functions.saveGuild(client, guildDoc);
                 });
-                */
+
             })
-            .catch(console.error);
+            .catch(
+                console.error
+            );
     }
 }
