9bd685abccd4d049af35cbec623e1c48762b897f
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-10-13 13:29:03 +1300

    feat: added soups

--- src/commands/eggs/execute/nest.js
+++ src/commands/eggs/execute/nest.js
@@ -10,6 +10,6 @@ module.exports = {
         // Loads the memberDoc from MongoDB (MongoDB is awesome ^-^);
         // Reply with the amount of messages the target user has
         await client.functions.getOrCreateUser(client, user.id)
-        .then(userDoc => (interaction.respond(interaction, `${client.emotes.sparkles} <@${user.id}> currently has ${userDoc.eggs} eggs!`)));
+        .then(userDoc => (interaction.respond(interaction, `${client.emotes.sparkles} <@${user.id}> currently has ${userDoc.eggs} eggs and ${userDoc.soups} soups!`)));
     }
 }
