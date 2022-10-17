85504ceb7cbbe179ad8d54c8d8f06d597982abd1
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-10-17 18:22:28 +1300

    fix: fixed problem with undefined doc

--- src/commands/eggs/execute/cook.js
+++ src/commands/eggs/execute/cook.js
@@ -4,21 +4,22 @@
 
 module.exports = {
     async execute(client, interaction) {
-        let userDoc = await client.functions.getOrCreateUser(client, interaction.user.id);
-        if (userDoc.eggs < 150) return await interaction.respond(interaction, `${client.emotes.x} Not enough eggs, 150 eggs required, you have ${userDoc.eggs} eggs`);
+        let userDoc = await client.functions.getOrCreateUser(client, interaction.user.id, async function (userDoc) {
+            if (userDoc.eggs < 150) return await interaction.respond(interaction, `${client.emotes.x} Not enough eggs, 150 eggs required, you have ${userDoc.eggs} eggs`);
 
-        let randomNo = Math.floor(Math.random() * 100);
-        if (randomNo < 50) {
-            userDoc.eggs = userDoc.eggs - randomNo * 2;
+            let randomNo = Math.floor(Math.random() * 100);
+            if (randomNo < 50) {
+                userDoc.eggs = userDoc.eggs - randomNo * 2;
+                await client.functions.saveUser(client, userDoc);
+                return await interaction.respond(interaction, `${client.emotes.warning} Oop, you cracked some of your eggs, you lost ${randomNo * 2} eggs !`);
+            }
+
+            userDoc.eggs = userDoc.eggs - 150;
+            userDoc.soups === undefined ? userDoc.soups = 1 : userDoc.soups = userDoc.soups + 1;
+
+            await interaction.respond(interaction, `You successfully made an egg soup! ${client.emotes.sparkles}, you have ${userDoc.soups} soups and ${userDoc.eggs} eggs`);
+
             await client.functions.saveUser(client, userDoc);
-            return await interaction.respond(interaction, `${client.emotes.warning} Oop, you cracked some of your eggs, you lost ${randomNo * 2} eggs !`);
-        }
-
-        userDoc.eggs = userDoc.eggs - 150;
-        userDoc.soups === undefined ? userDoc.soups = 1 : userDoc.soups = userDoc.soups + 1;
-
-        await interaction.respond(interaction, `You successfully made an egg soup! ${client.emotes.sparkles}, you have ${userDoc.soups} soups and ${userDoc.eggs} eggs`);
-
-        await client.functions.saveUser(client, userDoc);
+        });
     }
 }
