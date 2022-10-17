d9e254cc2bcb2265d7d5e810635ae3ef29523259
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-10-17 18:22:08 +1300

    fix: fixed problem with undefined user

--- src/commands/eggs/execute/collect.js
+++ src/commands/eggs/execute/collect.js
@@ -4,20 +4,22 @@
 
 module.exports = {
     async execute(client, interaction) {
-        let memberDoc = await client.functions.getOrCreateMember(client, interaction.guild.id, interaction.user.id);
-        /*
-        console.log(memberDoc);
-        console.log(await memberDoc.lastCollectedDate + await memberDoc.lastCollectedDate < Date.now() - 43200000);
-        */
-       memberDoc.lastCollectedDate === null ? console.log('true') : memberDoc.lastCollectedDate = null ;
-
-        if (typeof memberDoc.lastCollectedDate == null || memberDoc.lastCollectedDate < Date.now() - 43200000) {
-            await client.functions.addOrRemoveEggs(client, 100, interaction.user);
-            memberDoc.lastCollectedDate = new Date();
-            await client.functions.saveMember(client, memberDoc);
-            await interaction.respond(interaction, `${client.emotes.arrowUp} You have collected your daily 100 eggs successfully!`)
-        } else {
-            await interaction.respond(interaction, `${client.emotes.x} You have already collected your daily eggs`);
-        }
+        await client.functions.getOrCreateMember(client, interaction.guild.id, interaction.user.id, async function(memberDoc) {
+            console.log('after getting, memberdoc is ' + memberDoc);
+            /*
+            console.log(memberDoc);
+            console.log(await memberDoc.lastCollectedDate + await memberDoc.lastCollectedDate < Date.now() - 43200000);
+            */
+           //memberDoc.lastCollectedDate === null ? console.log('true') : memberDoc.lastCollectedDate = Date.now() ;
+    
+            if (typeof memberDoc.lastCollectedDate == null || memberDoc.lastCollectedDate < Date.now() - 43200000) {
+                await client.functions.addOrRemoveEggs(client, 100, interaction.user);
+                memberDoc.lastCollectedDate = new Date();
+                await client.functions.saveMember(client, memberDoc);
+                await interaction.respond(interaction, `${client.emotes.arrowUp} You have collected your daily 100 eggs successfully!`)
+            } else {
+                await interaction.respond(interaction, `${client.emotes.x} You have already collected your daily eggs for this server`);
+            }
+        })
     }
 }
